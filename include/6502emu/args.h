2636320ce1f11003c0305df7c16f20187cbb92a1
rfmineguy <rfmineguy@gmail.com>
2023-08-19 22:56:41 -0700

    Added cpp extern C to args.h

--- include/6502emu/args.h
+++ include/6502emu/args.h
@@ -1,11 +1,19 @@
 #ifndef ARGS_H
 #define ARGS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
   const char* input_file;
 } args_t; 
 
 args_t args_parse(int argc, char** argv);
 void   args_usage();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
